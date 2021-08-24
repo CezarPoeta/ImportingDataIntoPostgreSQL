f94e7f97d612df1ecdbf3a1621ba82a1684acbb5
Cezar Roberto Poeta <cezarrobertopoeta@gmail.com>
2021-08-24 08:42:28 -0300

    Update index - 01 -TBBENEFICIARIOS.js
    Updated

--- app/index - 01 -TBBENEFICIARIOS.js
+++ app/index - 01 -TBBENEFICIARIOS.js
@@ -7,7 +7,7 @@ today = new Date();
 
 const client = new pg.Client({                //Conexão com o Banco sysfinanctrl
     host: 'localhost',
-    database: 'PersonalFinance',
+    database: 'sysfinanctrl',
     user: 'cpoeta',
     password: '@58415433P',
     port: 5432,
@@ -29,9 +29,9 @@ fs.createReadStream(__dirname + '/src/TBBENEFICIARIOS.csv').pipe(parse({delimite
     var nl = 0
     for (var i = 1;i < csvData.length;i++) {
 
-        pSql = `INSERT INTO tbBeneficiarios (nome, created, updated, active, useradm_id) VALUES `
+        pSql = `INSERT INTO tbbeneficiarios (nome, created, updated) VALUES `
         pSql = `${pSql} ('${csvData[i][1].trim()}'`
-        pSql = `${pSql}, '${dt(today)}', '${dt(today)}', True, 1)`
+        pSql = `${pSql}, '${dt(today)}', '${dt(today)}')`
 
         client.query(pSql, function (err, rows, fields) {
             if (!err) {
